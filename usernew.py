783497b5e26050a23ea21192c4808e34d0a61092
Progameitor <javigarzas27@gmail.com>
2019-12-03 09:22:20 +0100

    sacar recomendador

--- usernew.py
+++ usernew.py
@@ -8,6 +8,7 @@ import nltk # install nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer 
 sid = SentimentIntensityAnalyzer()
 nltk.download('vader_lexicon')
+import json
 
 # retornas toda la informacion de la api
 @get("/")
@@ -72,9 +73,9 @@ def chatsWant(x,y):
     return chats_want
 
 
-@post('/chat/<chat_id>/addmessage')
+'''@post('/chat/<chat_id>/addmessage')
 def addMessage():
-    def createMessage(chat_id):
+    
     datauser, user = mc.connectCollection("apiproject","users")
     regis = list(user.aggregate([{'$project':{'userName':1, 'idUser':1,'_id':0}}]))
     name = str(request.forms.get("name"))
@@ -97,78 +98,13 @@ def addMessage():
             new_message['userName'] = name
             
     coll.insert_one(new_message)
-
+'''
  
 
-@get("/sentimenttext")
-def sentimentalltext():
-    database, collection =connectCollection('chats','chateo')
-    sentiment = {}
-    sentimental =dumps(collection.find({},{"text":1}))
-    sentiment["analysis"]=TextBlob(sentimental).sentiment
-    return sentiment
-
-
-
-@get("/sentimentchat=<x>")
-def sentimentchat(x):
-    database, collection =connectCollection('chats','chateo')
-    x = int(x)
-    sentitext = {}
-    sentitext_want = []
-    for a in range(x):
-        sentitext_want.append(dumps(collection.find({"idChat":x},{"text":1, "idChat":1})))
-        sentitext["analyze"]=TextBlob(str(sentitext_want)).sentiment
-    return sentitext
-
-#HASTA AQUI TODOS FUNCIONAN 
-
-@get("/sentimentuser=<x>,<y>")
-def sentimentuser(x,y):
-    database, collection =connectCollection('chats','chateo')
-    x = int(x)
-    y = int(y)
-    sentitextuser= {}
-    sentitext_user = []
-    for a in range(x,y):
-        sentitext_user.append(dumps(collection.find({"idChat":x, "idUser":y},{"text":1,"idChat":1, "userName":1})))
-        sentitextuser["user"]=TextBlob(str(sentitext_user)).sentiment
-    return sentitextuser
-
-# analizar con nltk todos los textos
-@get("/nltksentimenttext")
-def nltktext():
-    database, collection =connectCollection('chats','chateo')
-    nltk_sentiment = {}
-    nltk_sentimental =dumps(collection.find({},{"text":1}))
-    nltk_sentiment["nltk_analysis"]=sid.polarity_scores(nltk_sentimental)
-    return nltk_sentiment
-
-# analizar por idChat para analizar cada chat por separado con nltk
-@get("/nltksentimentchat=<x>")
-def nltkchat(x):
-    database, collection =connectCollection('chats','chateo')
-    nltk_sentitext = {}
-    nltk_sentitext_want = []
-    x = int(x)
-    for a in range(x):
-        nltk_sentitext_want.append(dumps(collection.find({"idChat":x},{"text":1, "idChat":1})))
-        nltk_sentitext["analyze"]=sid.polarity_scores(str(nltk_sentitext_want))
-    return nltk_sentitext
-
-
-# analizar por idChat para analizar cada chat por separado y usuario con nltk
-@get("/nltksentimentchatuser=<x>,<y>")
-def nltkuser(x,y):
-    database, collection =connectCollection('chats','chateo')
-    x = int(x)
-    y = int(y)
-    nltk_sentitextuser= {}
-    nltk_sentitext_user = []
-    for a in range(x,y):
-        nltk_sentitext_user.append(dumps(collection.find({"idChat":x, "idUser":y},{"text":1,"idChat":1, "userName":1})))
-        nltk_sentitextuser["user"]=sid.polarity_scores(str(nltk_sentitext_user))
-    return nltk_sentitextuser
-
+@get("/usertext")
+def getAllUsers():
+    database, collection = connectCollection('chats','chateo')
+    """Returns all users"""
+    return dumps(collection.find({},{'userName':1,"text":1,'_id':0})) 
 
 run(host="localhost", port=9090, debug=True)
